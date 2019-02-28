897ba7901fe7f3ef476d12052a023a42697cba2b
AdrewBraz <andbrazhnikov@gmail.com>
2019-02-28 16:30:58 +0300

    Fixed problems in evenGame.js

--- src/evenGame.js
+++ src/evenGame.js
@@ -2,21 +2,27 @@ import readlineSync from 'readline-sync';
 
 const getName = () => readlineSync.question('May I have your name?');
 
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = num => num % 2 === 0;
 
 const getAnswer = num => readlineSync.question(`Question: ${num}`);
 
 const startGame = (name, counter = 0) => {
-  const num = Math.round(Math.random() * 10);
-  const answer = getAnswer(num);
-  console.log(`Your answer: ${answer}`);
-  if (isEven(num) !== answer) {
-    return console.log(`${answer} is the wrong answer ;(. Correct answer was ${isEven(num)}
+  const randomNumber = Math.round(Math.random() * 10);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  const userAnswer = getAnswer(randomNumber);
+  console.log(`Your answer: ${userAnswer}`);
+  if (correctAnswer !== userAnswer) {
+    console.log(`${userAnswer} is the wrong answer ;(. Correct answer was ${correctAnswer}
     Let's try again, ${name}!`);
+    return;
   }
   console.log('Correct!');
   const count = counter + 1;
-  return count === 3 ? console.log(`Congratulations, ${name}!`) : startGame(name, count);
+  if (count === 3) {
+    console.log(`Congratulations, ${name}!`);
+    return;
+  }
+  startGame(name, count);
 };
 
 const evenGame = () => {
